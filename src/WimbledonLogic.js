0e6c08ddf22e2a3909a1c043db7dcccf9712b785
Jac0310 <jthoma24@dcs.bbk.ac.uk>
2019-03-01 16:10:51 +0000

    displaying data

--- src/WimbledonLogic.js
+++ src/WimbledonLogic.js
@@ -3,52 +3,62 @@ function search() {
     $.getJSON("wimbledon-men-2013.json", function (json) {
         $.each(json, function (key, value)
         {
-            // alert(i + " " +  obj.toString()
-            // )
             read(key, value)
         })
-
     })
-
-        
-
-    // $.each(json.)
 }
-function getjson() {
-    return $.getJSON( "wimbledon-men-2013.json");
-}
+
 
 function read(key, value)
 {
     alert(key + " " +  value)
     if (key === "match") {
-        var matches = $.each(value, function (k1, v1) {
-            // $.each(v, function (k1, v1) {
-            //     read(k1, v1)
-            //
-            // })
+        $.each(value, function (k1, v1) {
             matchnumber = k1
             match = v1
+            buildMatchRows(match)
 
         })
     }
 }
 
-function buildMatchRows(round, players)
+function buildMatchRows(match)
 {
+    var row1 = "<tr><td>"
+    var row2 = "<tr><td>"
+    $.each(match, function (k, v) {
+        if (k === "round") {row1 += v; row2 += v}
+        if (k === "player") {
+            $.each(v, function (playernumber, playerdata) {
 
+                if (playernumber === 0)
+                {
+                    buildRow(row1, playerdata)
+                }
+                else {
+                    buildRow(row2, playerdata)
+                }
+            })
+        }
+
+    })
 }
 
-// function tableOfSquares() {
-// // Display a prompt with a zero in it
-//     var num = window.prompt("Enter an integer", "0");
-//     var myTable = "<table class='border figure'>";
-//     var count = 0;
-//     while(count < num) {
-//         // Each row of the table is an integer and its square
-//         myTable = myTable + "<tr><td>" + count + "</td><td>"
-//             + count*count + "</td></tr>";
-//         count++;
-//     }
-//     document.getElementById("tos").innerHTML = myTable + "</table>";
-// }
+function buildRow(row, playerdata)
+{
+    row += "</td><td>"
+    $.each(playerdata, function (k,v) {
+
+        if (k === "name") { row += v};
+        if (k === "set") {
+            var i;
+            for ( i = 0; i < v.length; i++)
+            {
+                row += "</td><td>" + v[i];
+            }
+        }
+    })
+    row += "</td><tr>"
+    $("#display").append(row)
+}
+
